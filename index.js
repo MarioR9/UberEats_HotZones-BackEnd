62454cd4549faa66df3256df74eb1c529398f559
marior9 <mariorodriguezan@gmail.com>
2019-12-31 13:48:08 -0500

    basic server

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+app.listen(3000, ()=>console.log('listeing at 3000'));
+
+app.post('/location', (request, response)=>{
+    console.log('i got a response');
+    console.log(request.body);
+    response.header("Access-Control-Allow-Origin", "*");
+    response.json({
+        status: "success",
+        test: "123"
+    })
+    
+})
+
